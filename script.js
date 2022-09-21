93ba0dc658ed0a9769ef15dd2af6b0e6c368366c
benefitpets <114049156+benefitpets@users.noreply.github.com>
2022-09-21 22:40:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,18 +29,3 @@ function menu() {
     else
         $(menu).css("display", "block");
 }
-
-window.addEventListener('load', function(e) {
-
-  window.applicationCache.addEventListener('updateready', function(e) {
-    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-      // Browser downloaded a new app cache.
-      // Swap it in and reload the page to get the new hotness.
-      window.applicationCache.swapCache();
-        window.location.reload();
-    } else {
-      // Manifest didn't changed. Nothing new to server.
-    }
-  }, false);
-
-}, false);
